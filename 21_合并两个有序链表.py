1ac1a655f19dcff07e8df40aed7ed1a44a135c22
lovehhf <853885165@qq.com>
2019-04-18 23:50:24 +0800

    20190418

--- 21_合并两个有序链表.py
+++ 21_合并两个有序链表.py
@@ -1,85 +1,39 @@
-# Definition for singly-linked list.
+# -*- coding:utf-8 -*-
+
+__author__ = 'huanghf'
+
+"""
+题目描述
+输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+"""
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
+
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        phead = ListNode(0)
-        p = phead
-
-        while l1 and l2:
-            if l1.val < l2.val:
-
-                p.next = l1
-                l1 = l1.next
-                return l1.next
-
+    # 返回合并后列表
+    def Merge(self, pHead1, pHead2):
+        """
+        :param pHead1:
+        :param pHead2:
+        :return:
+        """
+        pHead = ListNode(0)
+        p = pHead
+        while pHead1 and pHead2:
+            if pHead1.val < pHead2.val:
+                pHead.next = pHead1
+                pHead1 = pHead1.next
             else:
-                p.next = l2
-                l2 = l2.next
-                return l2.next
-            p = p.next
-        if l1:
-            p.next = l1
-        if l2:
-            p.next = l2
-        return phead.next
-
-
-def stringToIntegerList(input):
-    return json.loads(input)
-
-
-def stringToListNode(input):
-    # Generate list from the input
-    numbers = stringToIntegerList(input)
-
-    # Now convert that list into linked list
-    dummyRoot = ListNode(0)
-    ptr = dummyRoot
-    for number in numbers:
-        ptr.next = ListNode(number)
-        ptr = ptr.next
-
-    ptr = dummyRoot.next
-    return ptr
-
-
-def listNodeToString(node):
-    if not node:
-        return "[]"
-
-    result = ""
-    while node:
-        result += str(node.val) + ", "
-        node = node.next
-    return "[" + result[:-2] + "]"
-
-
-def main():
-    import sys
-    import io
-    def readlines():
-        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
-            yield line.strip('\n')
-
-    lines = readlines()
-    while True:
-        try:
-            line = next(lines)
-            l1 = stringToListNode(line);
-            line = next(lines)
-            l2 = stringToListNode(line);
-
-            ret = Solution().mergeTwoLists(l1, l2)
-
-            out = listNodeToString(ret);
-            print(out)
-        except StopIteration:
-            break
-
-
-if __name__ == '__main__':
-    main()
+                pHead.next = pHead2
+                pHead2 = pHead2.next
+            pHead = pHead.next
+        if pHead1:
+            pHead.next = pHead1
+        if pHead2:
+            pHead.next = pHead2
+        return p.next
