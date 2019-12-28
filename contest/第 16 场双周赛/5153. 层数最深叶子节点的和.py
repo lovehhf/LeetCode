8375a3ca31842dee27c7937e5ea1f3371304c913
lovehhf <853885165@qq.com>
2019-12-29 01:31:31 +0800

    添加bfs解法

--- contest/第 16 场双周赛/5153. 层数最深叶子节点的和.py
+++ contest/第 16 场双周赛/5153. 层数最深叶子节点的和.py
@@ -19,7 +19,36 @@
 每个节点的值在 1 到 100 之间。
 
 mid
-只想到了bfs的解法
+
+
+dl的dfs解法:
+
+class Solution {
+public:
+    int md, res;
+    void dfs(TreeNode* x , int dep) {
+        if (x == NULL) return;
+        if (x->left == NULL && x->right == NULL) {
+            if (dep > md) {
+                res = 0;
+                md = dep;
+            }
+            if (dep == md) {
+                res += x->val;
+            }
+        } else {
+            dfs(x->left, dep + 1);
+            dfs(x->right, dep + 1);
+        }
+    }
+
+    int deepestLeavesSum(TreeNode* root) {
+        md = -1;
+        res = 0;
+        dfs(root , 0);
+        return res;
+    }
+};
 """
 
 # Definition for a binary tree node.
@@ -34,6 +63,11 @@ from utils.TreeNode import TreeNode
 
 class Solution:
     def deepestLeavesSum(self, root: TreeNode) -> int:
+        """
+        bfs层次遍历
+        :param root:
+        :return:
+        """
         s = [0] * 10010
         q = [(root, 0)]
         level = 0
@@ -46,3 +80,31 @@ class Solution:
             if not cur.left and not cur.right:
                 s[level] += cur.val
         return s[level]
+
+
+class DFS_Solution:
+    """
+    dfs 解法
+    """
+
+    def __init__(self):
+        self.max_depth = 0
+        self.res = 0
+
+    def dfs(self, node, depth):
+        if not node:
+            return
+
+        if not node.left and not node.right:
+            if depth == self.max_depth:
+                self.res += node.val
+        else:
+            if depth + 1 > self.max_depth:
+                self.max_depth = depth + 1
+                self.res = 0
+            self.dfs(node.left, depth + 1)
+            self.dfs(node.right, depth + 1)
+
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        self.dfs(root, 0)
+        return self.res
