b47bd42cce02a810ba0c92b80120df8b4995fd5d
lovehhf <853885165@qq.com>
2020-01-11 00:38:16 +0800

    感觉写的不怎么好, 待改进

---
+++ 384. 打乱数组.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+
+"""
+打乱一个没有重复元素的数组。
+
+示例:
+
+// 以数字集合 1, 2 和 3 初始化数组。
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。
+solution.shuffle();
+
+// 重设数组到它的初始状态[1,2,3]。
+solution.reset();
+
+// 随机返回数组[1,2,3]打乱后的结果。
+solution.shuffle();
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shuffle-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+import random
+from typing import List
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        n = len(self.nums)
+        idxs = list(range(n))
+        ret = []
+        while idxs:
+            ret.append(self.nums[idxs.pop(random.randint(0, n - 1))])
+            n -= 1
+        return ret
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
