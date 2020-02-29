c9012077f4f4cc5f4f3ca8c3bd4b0738296e90b5
huanghongfei <853885165@qq.com>
2020-03-01 01:16:14 +0800

    [update] 改为使用1个队列实现栈
    四种操作实现如下：
    
    - `push(x)` : 直接插入队尾
    - `pop()`:  弹出队尾元素。将队首元素弹出并插入队尾，循环 n-1 次，n 是队列长度。此时队尾元素已经在队首了，直接将其弹出即可；
    - `top()`:  直接队尾元素
    - `empty()`: 返回队列是否为空

--- 225_用队列实现栈.py
+++ 225_用队列实现栈.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
 
-__author__ = 'huanghf'
-
 """
 使用队列实现栈的下列操作：
 
@@ -9,8 +7,8 @@ push(x) -- 元素 x 入栈
 pop() -- 移除栈顶元素
 top() -- 获取栈顶元素
 empty() -- 返回栈是否为空
-注意:
 
+注意:
 你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
 你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
 你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
@@ -19,54 +17,44 @@ empty() -- 返回栈是否为空
 from collections import deque
 
 
-class MyStack(object):
+class MyStack:
 
     def __init__(self):
         """
-        self.queue1:存放队列元素
-        self.queue2:pop时用于暂时存放队列的前n-1个元素
-        self.n:队列长度
+        队列的弹出队头元素和插入到队尾的时间复杂度均是O(1), 使用一个队列就可以实现栈, 没有必要用2个
         Initialize your data structure here.
         """
-        self.queue1 = deque([])
-        self.queue2 = deque([])
+        self.queue = deque([])
 
-    def push(self, x):
+    def push(self, x: int) -> None:
         """
+        直接插入到队尾
         Push element x onto stack.
-        :type x: int
-        :rtype: None
         """
-        self.queue1.append(x)
+        self.queue.append(x)
 
-    def pop(self):
+    def pop(self) -> int:
         """
-        将队列1的元素除了最后一个以外都倒到队列2里面去
-        遍历记录队列1最后的元素
-        再将队列2的元素倒回队列1
+        将队头元素插入到队尾, 循环 n - 1 次, 最后弹出队头元素
         Removes the element on top of the stack and returns that element.
-        :rtype: int
         """
-        while len(self.queue1) > 1:
-            self.queue2.append(self.queue1.popleft())
-        top_element = self.queue1.popleft()
-        while self.queue2:
-            self.queue1.append(self.queue2.popleft())
-        return top_element
+        n = len(self.queue)
+        for _ in range(n - 1):
+            self.queue.append(self.queue.popleft())
+        return self.queue.popleft()
 
-    def top(self):
+    def top(self) -> int:
         """
+        直接返回队尾元素
         Get the top element.
-        :rtype: int
         """
-        return self.queue1[-1]
+        return self.queue[-1]
 
-    def empty(self):
+    def empty(self) -> bool:
         """
         Returns whether the stack is empty.
-        :rtype: bool
         """
-        return not self.queue1
+        return not self.queue
 
 
 stack = MyStack()
