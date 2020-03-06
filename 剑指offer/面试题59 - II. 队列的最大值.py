8eb18b96651a7a96801cf0bce09cd8bcbf60dd25
huanghongfei <853885165@qq.com>
2020-03-07 01:14:35 +0800

    [UPDATE] 队列的最大值, 使用双端队列优化了一下代码

--- 剑指offer/面试题59 - II. 队列的最大值.py
+++ 剑指offer/面试题59 - II. 队列的最大值.py
@@ -26,36 +26,39 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
+from collections import deque
 
 class MaxQueue:
 
     def __init__(self):
-        self.queue = []
-        self.max_queue = []  # 单调非增队列, 只存储单调非增的元素的元素 队列是先进先出的结构
-                             # 如果 先进的值比后进的值少, 先进的值就永无出头之日, eg [1, 3, 2], 因为有 3 存在, 1 在出队列之间都不可能会是最大值, max_queue = [3, 2]
+        self.queue = deque([])
+        self.max_queue = deque([])  # 单调非增队列, 只存储单调非增的元素的元素， 队头元素是队列的最大值
+                              # 如果先进的值比后进的值小, 先进的值就永无出头之日, eg [1, 3, 2], 因为有 3 存在, 1 在出队列之间都不可能会是最大值, max_queue = [3, 2]
 
     def max_value(self) -> int:
-        if not self.max_queue:
-            return -1
-        return self.max_queue[0]
+        return self.max_queue[0] if self.max_queue else -1
 
     def push_back(self, value: int) -> None:
         """
-        这里似乎不是 O(1)
         :param value:
         :return:
         """
         self.queue.append(value)
+
+        # 从后往前弹出单调队列中比value值小的元素
         while self.max_queue and self.max_queue[-1] < value:
             self.max_queue.pop()
+
         self.max_queue.append(value)
 
     def pop_front(self) -> int:
         if not self.queue:
             return -1
-        val = self.queue.pop(0)
+
+        val = self.queue.popleft()
         if val == self.max_queue[0]:
-            self.max_queue.pop(0)
+            self.max_queue.popleft()
+
         return val
 
 # Your MaxQueue object will be instantiated and called as such:
