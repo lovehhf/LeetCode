a43a4f176b2e0d0d833bc739f3fb16fdcead6d4f
huanghongfei <853885165@qq.com>
2020-03-11 01:52:13 +0800

    [UPDATE] 48. 旋转图像, 顺时针与逆时针旋转方阵
    先局部翻转行或列, 再交叉互换

--- 48_旋转图像.py
+++ 48_旋转图像.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
 
-__author__ = 'huanghf'
-
 """
 给定一个 n × n 的二维矩阵表示一个图像。
 将图像顺时针旋转 90 度。
@@ -22,72 +20,71 @@ __author__ = 'huanghf'
   [8,5,2],
   [9,6,3]
 ]
+
+参考了: https://github.com/zzhznx/LeetCode-Python/blob/9dd98d903e492fc509f0096115caccfd4c9fe8e8/48-Rotate%20Image.py
 """
 
 
 class Solution(object):
-    '''
-    顺时针旋转
-    先翻转二维数组,再交叉对换
-    1 2 3     7 8 9     7 4 1
-    4 5 6  => 4 5 6  => 8 5 2
-    7 8 9     1 2 3     9 6 3
-    '''
+
     def rotate(self, matrix):
         """
+        '''
+        顺时针旋转
+        先翻转每一列,再交叉对换
+        1 2 3     7 8 9     7 4 1
+        4 5 6  => 4 5 6  => 8 5 2
+        7 8 9     1 2 3     9 6 3
+        '''
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()
-        for i in range(len(matrix)):
-            for j in range(i+1, len(matrix[0])):
-                # print([i,j],[j,i])
-                (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j])
-            # print("----------")
+        n = len(matrix)
 
-    def rotate2(self, A):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
-        print(list(zip(*A)))
-        # t = *A[::-1]
-        # print(A[:])
-        A[:] = zip(*A[::-1])
+        # 先翻转每一列
+        for j in range(n):
+            for i in range(n // 2):
+                matrix[i][j], matrix[n - 1 - i][j] = matrix[n - 1 - i][j], matrix[i][j]
+
+        # 再交叉互换
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+class AntiRotateSolution(object):
 
     def anti_rotate(self, matrix):
         """
         逆时针旋转
-        anticlockwise rotate
-        first reverse left to right, then swap the symmetry
+        先翻转每一行, 再翻转每一列
         1 2 3     3 2 1     3 6 9
         4 5 6  => 6 5 4  => 2 5 8
         7 8 9     9 8 7     1 4 7
-        https://github.com/zzhznx/LeetCode-Python/blob/9dd98d903e492fc509f0096115caccfd4c9fe8e8/48-Rotate%20Image.py
         :param matrix:
         :return:
         """
-        for m in matrix:
-            m[:] = m[::-1]
-        for i in range(len(matrix)):
-            for j in range(i + 1, len(matrix[0])):
-                (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j])
-        return matrix
+        n = len(matrix)
 
-    def anti_rotate_with_zip(self, matrix):
-        """逆时针旋转 使用zip"""
-        for m in matrix:
-            m[:] = m[::-1]
-        matrix[:] = zip(*matrix)
-        return matrix
+        # 翻转每一行
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
 
-matrix = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
+        # 交叉互换
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 
-s = Solution()
-s.rotate2(matrix)
-print(matrix)
+if __name__ == '__main__':
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    s = Solution()
+    s.rotate(matrix)
+    print(matrix)
+
+    s2 = AntiRotateSolution()
+    s2.anti_rotate(matrix)
+
+    print(matrix)
