307527a30cbf10604f3ea19018dc68a5cbfa2886
huanghongfei <853885165@qq.com>
2020-03-14 05:42:50 +0800

    [UPDATE] 300. 最长上升子序列, 贪心+二分将时间复杂度优化到O(nlogn))

--- 300_最长上升子序列.py
+++ 300_最长上升子序列.py
@@ -6,36 +6,43 @@ __author__ = 'huanghf'
 给定一个无序的整数数组，找到其中最长上升子序列的长度。
 
 示例:
-
 输入: [10,9,2,5,3,7,101,18]
 输出: 4 
 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
-说明:
 
+说明:
 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
 你算法的时间复杂度应该为 O(n2) 。
 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
 """
 
+from typing import List
 
-class Solution(object):
-    def lengthOfLIS(self, nums):
+
+class Solution:
+    def find(self, nums, target):
         """
-        dp[i]: 以nums[i]结尾的最长上升子序列的长度
-        状态转移方程: dp[i] = max(dp[i], dp[j] + 1)  for j in range(i) if nums[i] > nums[j]
-        时间复杂度O(n^2)
-        :type nums: List[int]
-        :rtype: int
+        二分查找nums中第一个 >= target 的索引
         """
-        if not nums:
-            return 0
         n = len(nums)
-        dp = [1] * n
-        for i in range(1, n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+        l, r = 0, n - 1
+        while (l < r):
+            mid = (l + r) >> 1
+            if (nums[mid] >= target):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        f = [0x3f3f3f3f] * n  # 辅助数组, 只存最优的递增的子序列
+        res = 0
+        for i in range(n):
+            j = self.find(f, nums[i])  # 找到第一个 >= nums[i] 的数并更新辅助数组
+            f[j] = nums[i]
+            res = max(res, j + 1)
+        return res
 
 
 nums = [10, 9, 2, 5, 3, 7, 101, 18]
