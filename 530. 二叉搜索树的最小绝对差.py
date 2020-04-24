e79551a8fa4ab75429eafe98d31f8b4eeff4ff99
huanghongfei <853885165@qq.com>
2020-04-24 22:48:28 +0800

    [ADD] 530. 二叉搜索树的最小绝对差， 中序遍历

---
+++ 530. 二叉搜索树的最小绝对差.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+链接：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst
+
+给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+
+示例：
+输入：
+
+   1
+    \
+     3
+    /
+   2
+
+输出：
+1
+
+解释：
+最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+
+提示：
+树中至少有 2 个节点。
+本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/ 相同
+"""
+from utils.TreeNode import TreeNode
+
+
+class Solution:
+    def inorder(self, root):
+        if not root:
+            return []
+        return self.inorder(root.left) + [root.val] + self.inorder(root.right)
+
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        nums = self.inorder(root)
+        return min(nums[i] - nums[i - 1] for i in range(1, len(nums)))
