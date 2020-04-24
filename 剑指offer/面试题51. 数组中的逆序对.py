edc46bf58d7de9dacb2ca2b7a70f2d399ec2dfbe
huanghongfei <853885165@qq.com>
2020-04-24 22:49:56 +0800

    [ADD] 面试题51. 数组中的逆序对, 归并排序模板

--- 剑指offer/面试题51. 数组中的逆序对.py
+++ 剑指offer/面试题51. 数组中的逆序对.py
@@ -14,3 +14,45 @@
 链接：https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+from typing import List
+
+
+class Solution:
+    def merge_sort(self, nums, l, r):
+        if (l >= r):
+            return 0
+
+        tmp = []
+        mid = (l + r) >> 1
+
+        res = 0
+        res += self.merge_sort(nums, l, mid)
+        res += self.merge_sort(nums, mid + 1, r)
+
+        i = l
+        j = mid + 1
+        while (i <= mid and j <= r):
+            if nums[i] <= nums[j]:
+                tmp.append(nums[i])
+                i += 1
+            else:
+                res += mid - i + 1
+                tmp.append(nums[j])
+                j += 1
+
+        if (i <= mid):
+            tmp += nums[i:mid + 1]
+        if (j <= r):
+            tmp += nums[j:r + 1]
+
+        nums[l: r + 1] = tmp
+        return res
+
+    def reversePairs(self, nums: List[int]) -> int:
+        return self.merge_sort(nums, 0, len(nums) - 1)
+
+
+s = Solution()
+nums = [1,3,2,3,1]
+print(s.reversePairs(nums))
