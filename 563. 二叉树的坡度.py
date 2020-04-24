ae6f1021b425c7fdfcbdd839557c64d7b9b47c00
huanghongfei <853885165@qq.com>
2020-04-24 22:47:59 +0800

    [ADD] 563. 二叉树的坡度, 树形dp, dfs

---
+++ 563. 二叉树的坡度.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+"""
+链接：https://leetcode-cn.com/problems/binary-tree-tilt
+
+给定一个二叉树，计算整个树的坡度。
+一个树的节点的坡度定义即为，该节点左子树的结点之和和右子树结点之和的差的绝对值。空结点的的坡度是0。
+整个树的坡度就是其所有节点的坡度之和。
+
+示例:
+输入:
+         1
+       /   \
+      2     3
+输出: 1
+解释:
+结点的坡度 2 : 0
+结点的坡度 3 : 0
+结点的坡度 1 : |2-3| = 1
+树的坡度 : 0 + 0 + 1 = 1
+
+注意:
+任何子树的结点的和不会超过32位整数的范围。
+坡度的值不会超过32位整数的范围。
+"""
+
+from utils.TreeNode import TreeNode
+
+
+class Solution:
+    def dfs(self, root):
+        if not root:
+            return 0
+        l = self.dfs(root.left)
+        r = self.dfs(root.right)
+        self.res += abs(l - r)
+        return l + r + root.val
+
+    def findTilt(self, root: TreeNode) -> int:
+        self.res = 0
+        self.dfs(root)
+        return self.res
