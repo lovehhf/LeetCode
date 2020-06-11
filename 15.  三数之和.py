b124e897d27625dad80b4613b32be0a9ea9e99a7
huanghongfei <853885165@qq.com>
2020-06-12 00:40:00 +0800

    [UPDATE] 15.  三数之和, 双指针

---
+++ 15.  三数之和.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+注意：答案中不可以包含重复的三元组。
+
+示例：
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+"""
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, n - 1
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif (nums[i] + nums[l] + nums[r] < 0):
+                    l += 1
+                else:
+                    r -= 1
+        return res
+
+
+s = Solution()
+nums = [3, 0, -2, -1, 1, 2]
+print(s.threeSum(nums))
